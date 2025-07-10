cd971ad04dfcca1744086800c912eb3a748b42cb
Viswadatta1233 <n210059@rguktn.ac.in>
2025-07-10 22:13:55 +0530

    port

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.use('/api/problems', problemRoutes);
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI)
     .then(() => {
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
